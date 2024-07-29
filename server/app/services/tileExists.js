6fa94725ce59de9d95e40803a37121745cc1105b
Megane <megane.loisele@gmail.com>
2024-07-29 11:55:44 +0200

    step4 ok

--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,1 +1,10 @@
-module.exports = null;
+const tileExists = (req, res, next) => {
+  if (req.body.coord_x <= 11 && req.body.coord_y <= 5) {
+    next();
+  } else {
+    res.sendStatus(422);
+  }
+};
+// Coordinates are invalid
+
+module.exports = tileExists;
