5ca7783a7f03a28931475385ab5ab396259b4675
Megane <megane.loisele@gmail.com>
2024-07-29 10:58:05 +0200

    step3 ok

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -12,7 +12,23 @@ const browse = async (req, res, next) => {
     next(err);
   }
 };
+const edit = async (req, res, next) => {
+  // Extract the boat data from the request body and params
+  const boat = { ...req.body, id: req.params.id };
+
+  try {
+    // Update the boat in the database
+    await tables.boat.update(boat);
+
+    // Respond with HTTP 204 (No Content)
+    res.sendStatus(204);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
 
 module.exports = {
   browse,
+  edit,
 };
--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -8,9 +8,18 @@ class BoatRepository extends AbstractRepository {
   async readAll() {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
     const [rows] = await this.database.query(`select * from ${this.table}`);
-
     // Return the array of boats
     return rows;
+  }
+
+  async update(boat) {
+    const query = `UPDATE ${this.table} SET coord_x = ?, coord_y = ? WHERE id = ?`;
+    const [result] = await this.database.execute(query, [
+      boat.coord_x,
+      boat.coord_y,
+      boat.id,
+    ]);
+    return result.affectedRows;
   }
 }
 
