ecd392b9bae367cafa3bae2f1a8da876e976151e
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-07-29 12:13:06 +0200

    finish step 4

--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,1 +1,16 @@
-module.exports = null;
+const TileRepository = require("../../database/models/TileRepository");
+
+const tileExists = async (req, res, next) => {
+  const tileRepository = new TileRepository();
+  const tiles = await tileRepository.readByCoordinates(
+    req.body.coord_x,
+    req.body.coord_y
+  );
+  if (tiles.length > 0) {
+    next();
+  } else {
+    res.sendStatus(422);
+  }
+};
+
+module.exports = tileExists;
